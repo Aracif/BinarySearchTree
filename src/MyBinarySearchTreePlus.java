682cd20df06bbbafea42667df873d37cebddd15b
ficar <ficar@DESKTOP-RS7CGLI.hsd1.nj.comcast.net>
2016-04-21 23:22:29 -0400

    Check

--- src/MyBinarySearchTreePlus.java
+++ src/MyBinarySearchTreePlus.java
@@ -1,4 +1,4 @@
-
+//INTEGER BASED
 public class MyBinarySearchTreePlus <T extends KeyedItem<KT>,KT extends Comparable<? super KT>> extends MyBinarySearchTree<T,KT> implements BSTPInterface{
 
 	public int getHeight(){
